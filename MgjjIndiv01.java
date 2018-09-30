57f7e9ca2738a6be7be4e2ed34252ba615b66a62
Srguindos <43728204+Srguindos@users.noreply.github.com>
2018-09-30 23:56:42 +0200

    Add files via upload

---
+++ MgjjIndiv01.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mgjjindiv01;
+
+/**
+ *
+ * @author Juanjo
+ */
+public class MgjjIndiv01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero=13;
+        System.out.println("Hola me llamo Juanjo");
+        
+        
+     System.out.println("variable para crear el numero + 13");
+    }
+    
+}
